d634734435d84236d5582dac016c2800c4a7a2bd
LydiahLaw <Lygashiku@gmail.com>
2026-03-04 17:30:23 +0300

    Fix: remove loose PBL folder from repo root

--- PBL/tooling.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install -y httpd php php-mysqlnd git
-systemctl start httpd
-systemctl enable httpd
--- PBL/wordpress.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install -y httpd php php-mysqlnd
-systemctl start httpd
-systemctl enable httpd
